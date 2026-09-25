c644679eeb5a4c44d3dc108b0e89bf811f8ca288
Henrique Jacobs Ortiz <seu-email@do-github.com>
2026-09-25 14:26:18 -0300

    Adiciona projeto.py

---
+++ projeto.py
@@ -0,0 +1,16 @@
+nome = "Seu Nome"
+idade = 20
+cidade = "Curitiba"
+linguagem = "Python"
+
+print("=" * 35)
+print("      MEU PROJETO PYTHON")
+print("=" * 35)
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Cidade: {cidade}")
+print(f"Linguagem favorita: {linguagem}")
+
+print("=" * 35)
+print("Obrigado por visitar meu projeto!")
